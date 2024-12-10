bca1f2f073d4f65e20b3c382fec9eee5c638107b
Fr3ddy1 <freddy.tapia.28@gmail.com>
2024-12-10 14:57:51 -0400

    add setup script

---
+++ setup.py
@@ -0,0 +1,15 @@
+
+from setuptools import setup
+
+setup(
+    name="prueba",
+    version="0.0.1",
+    description="Prueba package",
+    url="https://github.com/Fr3ddy12/prueba",
+    author="Freddy12",
+    packages=["prueba"],
+    install_requires=[
+        "pandas>=2.0.3",
+        "numpy>=1.23.5",
+    ],
+)
